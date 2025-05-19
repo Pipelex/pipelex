c7dbd38f94936b557f00a01d9c466f6c4573696d
antoinepipelex <antoine@pipelex.com>
2025-05-20 00:42:59 +0200

    Fix test

--- tests/tools/test_pdf.py
+++ tests/tools/test_pdf.py
@@ -10,7 +10,7 @@ from tests.test_data import PDFTestCases
 
 @pytest.mark.asyncio(loop_scope="class")
 class TestPDF:
-    @pytest.mark.parametrize("file_path", [PDFTestCases.DOCUMENT_FILE_PATHS])
+    @pytest.mark.parametrize("file_path", PDFTestCases.DOCUMENT_FILE_PATHS)
     async def test_pdf_path_to_images(self, file_path: str):
         images = await pypdfium2_renderer.render_pdf_pages(file_path)
-        assert len(images) == 1
+        assert len(images) > 0
