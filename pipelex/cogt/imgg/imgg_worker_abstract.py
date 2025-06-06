ce64ba91727d70b7a7bd49bc7d98d8d907c8a29e
Louis Choquel <louis@pipelex.com>
2025-06-06 23:33:43 +0200

    New wrapper for image generation methods

--- pipelex/cogt/imgg/imgg_worker_abstract.py
+++ pipelex/cogt/imgg/imgg_worker_abstract.py
@@ -12,44 +12,6 @@ from pipelex.cogt.inference.inference_worker_abstract import InferenceWorkerAbst
 from pipelex.pipeline.job_metadata import UnitJobId
 from pipelex.reporting.reporting_protocol import ReportingProtocol
 
-F = TypeVar("F", bound=Callable[..., Awaitable[Any]])
-
-
-def imgg_job_func(func: F) -> F:
-    @wraps(func)
-    async def wrapper(
-        self: Any,
-        imgg_job: ImggJob,
-        *args: Any,
-        **kwargs: Any,
-    ) -> Any:
-        log.debug(f"Working — {func.__name__} using:\n{self.imgg_engine.desc}")
-
-        # Verify that the job is valid
-        imgg_job.validate_before_execution()
-
-        # Verify feasibility
-        self.check_can_perform_job(imgg_job=imgg_job)
-        # TODO: check can generate object (where it will be appropriate)
-
-        # metadata
-        imgg_job.job_metadata.unit_job_id = UnitJobId.IMGG_TEXT_TO_IMAGE
-
-        # Prepare job
-        imgg_job.imgg_job_before_start(imgg_engine=self.imgg_engine)
-
-        # Execute job
-        result = await func(self, imgg_job, *args, **kwargs)
-
-        # Report job
-        imgg_job.imgg_job_after_complete()
-        if self.reporting_delegate:
-            self.reporting_delegate.report_inference_job(inference_job=imgg_job)
-
-        return result
-
-    return cast(F, wrapper)
-
 
 class ImggWorkerAbstract(InferenceWorkerAbstract):
     def __init__(
@@ -72,15 +34,72 @@ class ImggWorkerAbstract(InferenceWorkerAbstract):
     def check_can_perform_job(self, imgg_job: ImggJob):
         pass
 
-    @abstractmethod
     async def gen_image(
+        self,
+        imgg_job: ImggJob,
+    ) -> GeneratedImage:
+        log.debug(f"Image gen worker gen_image:\n{self.imgg_engine.desc}")
+
+        # Verify that the job is valid
+        imgg_job.validate_before_execution()
+
+        # Verify feasibility
+        self.check_can_perform_job(imgg_job=imgg_job)
+
+        # metadata
+        imgg_job.job_metadata.unit_job_id = UnitJobId.IMGG_TEXT_TO_IMAGE
+
+        # Prepare job
+        imgg_job.imgg_job_before_start(imgg_engine=self.imgg_engine)
+
+        # Execute job
+        result = await self._gen_image(imgg_job=imgg_job)
+
+        # Report job
+        imgg_job.imgg_job_after_complete()
+        if self.reporting_delegate:
+            self.reporting_delegate.report_inference_job(inference_job=imgg_job)
+
+        return result
+
+    @abstractmethod
+    async def _gen_image(
         self,
         imgg_job: ImggJob,
     ) -> GeneratedImage:
         pass
 
-    @abstractmethod
     async def gen_image_list(
+        self,
+        imgg_job: ImggJob,
+        nb_images: int,
+    ) -> List[GeneratedImage]:
+        log.debug(f"Image gen worker gen_image_list:\n{self.imgg_engine.desc}")
+
+        # Verify that the job is valid
+        imgg_job.validate_before_execution()
+
+        # Verify feasibility
+        self.check_can_perform_job(imgg_job=imgg_job)
+
+        # metadata
+        imgg_job.job_metadata.unit_job_id = UnitJobId.IMGG_TEXT_TO_IMAGE
+
+        # Prepare job
+        imgg_job.imgg_job_before_start(imgg_engine=self.imgg_engine)
+
+        # Execute job
+        result = await self._gen_image_list(imgg_job=imgg_job, nb_images=nb_images)
+
+        # Report job
+        imgg_job.imgg_job_after_complete()
+        if self.reporting_delegate:
+            self.reporting_delegate.report_inference_job(inference_job=imgg_job)
+
+        return result
+
+    @abstractmethod
+    async def _gen_image_list(
         self,
         imgg_job: ImggJob,
         nb_images: int,
--- tests/pipelex/conftest.py
+++ tests/pipelex/conftest.py
@@ -5,9 +5,9 @@ from pipelex.cogt.imgg.imgg_handle import ImggHandle
 
 @pytest.fixture(
     params=[
-        ImggHandle.FLUX_1_PRO_LEGACY,
-        ImggHandle.FLUX_1_1_PRO,
-        ImggHandle.FLUX_1_1_ULTRA,
+        # ImggHandle.FLUX_1_PRO_LEGACY,
+        # ImggHandle.FLUX_1_1_PRO,
+        # ImggHandle.FLUX_1_1_ULTRA,
         ImggHandle.SDXL_LIGHTNING,
         ImggHandle.OPENAI_GPT_IMAGE_1,
     ]
