280715ec39d8e7129f80607fc4bda08071b66ca0
thomashebrard <thomas@pipelex.com>
2025-06-26 10:31:46 +0200

    remove AnotherMockContent

--- tests/test_pipelines/test_content_factory.py
+++ tests/test_pipelines/test_content_factory.py
@@ -4,8 +4,3 @@ from pipelex.core.stuff_content import StructuredContent
 class MockRegisteredContent(StructuredContent):
     title: str
     description: str
-
-
-class AnotherMockContent(StructuredContent):
-    message: str
-    priority: int
