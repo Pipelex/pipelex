c99b6cdba2c5eb87d5b452e93631713707397e34
Louis Choquel <louis@pipelex.com>
2025-06-29 18:17:32 +0200

    Cleanup plugin config

--- pipelex/plugins/plugin_manager.py
+++ pipelex/plugins/plugin_manager.py
@@ -1,6 +1,7 @@
-from typing import Any, Optional, Type
+from typing import Any, Dict, Optional, Type
 
 from kajson.class_registry import ClassRegistry
+from pydantic import Field
 
 from pipelex.libraries.library_config import LibraryConfig
 from pipelex.plugins.plugin_sdk_registry import PluginSdkRegistry
@@ -12,6 +13,7 @@ class PluginManager:
     def __init__(self):
         self._plugin_configs: Optional[PluginConfig] = None
         self._plugin_registry = ClassRegistry()
+        self._llm_worker_classes: Dict[str, str] = dict()
         self.plugin_sdk_registry = PluginSdkRegistry()
 
     @property
@@ -26,10 +28,10 @@ class PluginManager:
         self._plugin_configs = PluginConfig.model_validate(plugin_config_dict)
 
     def get_required_plugin(self, plugin_name: str) -> Any:
-        plugin_class_name = self.plugin_configs.specific_llm_config.llm_worker_classes[plugin_name]
+        plugin_class_name = self._llm_worker_classes[plugin_name]
         plugin_class = self._plugin_registry.get_required_class(plugin_class_name)
         return plugin_class
 
     def register_plugin(self, name: str, plugin_class: Type[Any]):
-        self.plugin_configs.specific_llm_config.llm_worker_classes[name] = plugin_class.__name__
+        self._llm_worker_classes[name] = plugin_class.__name__
         self._plugin_registry.register_class(class_type=plugin_class)
