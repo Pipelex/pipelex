c1e9c2ccecdf85041f0e2bb4d6830b898e9da242
Louis Choquel <louis@pipelex.com>
2025-06-30 10:17:07 +0200

    factorized code for get_llm_model() and _get_llm_model_latest_version() which raise when the optional call fails

--- pipelex/cogt/llm/llm_models/llm_model_library.py
+++ pipelex/cogt/llm/llm_models/llm_model_library.py
@@ -84,21 +84,11 @@ class LLMModelLibrary(LLMModelProviderAbstract, RootModel[LLMModelLibraryRoot]):
         llm_version: str,
         llm_platform_choice: LLMPlatformChoice,
     ) -> LLMModel:
-        if llm_version == LATEST_VERSION_NAME:
-            return self._get_llm_model_latest_version(llm_name=llm_name, llm_platform_choice=llm_platform_choice)
-
-        found_llm_models = [llm_model for llm_model in self.root if llm_model.llm_name == llm_name and llm_model.version == llm_version]
-        if not found_llm_models:
+        llm_model = self.get_optional_llm_model(llm_name=llm_name, llm_version=llm_version, llm_platform_choice=llm_platform_choice)
+        if not llm_model:
             raise LLMModelProviderError(f"Model '{llm_name}' version '{llm_version}' not found")
-        if len(found_llm_models) > 1:
-            raise LLMModelProviderError(f"Model '{llm_name}' version '{llm_version}' has multiple instances in the library")
-        llm_model = found_llm_models[0]
-
-        if isinstance(llm_platform_choice, LLMPlatform):
-            llm_platform: LLMPlatform = llm_platform_choice
-            if llm_platform not in llm_model.platform_llm_id:
-                raise LLMModelPlatformError(f"Missing llm id for LLM model {llm_model.name_and_version}'s chosen platform='{llm_platform}'")
-        return llm_model
+        else:
+            return llm_model
 
     @override
     def get_optional_llm_model(
@@ -153,22 +143,10 @@ class LLMModelLibrary(LLMModelProviderAbstract, RootModel[LLMModelLibraryRoot]):
         llm_name: str,
         llm_platform_choice: LLMPlatformChoice,
     ) -> LLMModel:
-        found_llm_models = [llm_model for llm_model in self.root if llm_model.llm_name == llm_name]
-        if not found_llm_models:
-            raise LLMModelProviderError(f"Could not find model named '{llm_name}'")
-        elif len(found_llm_models) == 1:
-            # only one so it is the latest we've got
-            llm_model = found_llm_models[0]
-        elif chosen_model := next((model for model in found_llm_models if model.version == LATEST_VERSION_NAME), None):
-            # we've got a version named 'latest' so we use it
-            llm_model = chosen_model
-        elif isinstance(llm_platform_choice, LLMPlatform):
-            llm_platform: LLMPlatform = llm_platform_choice
-            llm_models_with_platform = [model for model in found_llm_models if llm_platform in model.platform_llm_id]
-            if not llm_models_with_platform:
-                raise LLMModelProviderError(f"Model '{llm_name}' has no version for platform '{llm_platform}'")
-            llm_model = max(llm_models_with_platform, key=lambda m: m.version)
+        llm_model_latest_version = self.get_optional_llm_model(
+            llm_name=llm_name, llm_version=LATEST_VERSION_NAME, llm_platform_choice=llm_platform_choice
+        )
+        if llm_model_latest_version is not None:
+            return llm_model_latest_version
         else:
-            assert llm_platform_choice == DEFAULT_PLATFORM_INDICATOR
-            llm_model = max(found_llm_models, key=lambda m: m.version)
-        return llm_model
+            raise LLMModelProviderError(f"Could not find model named '{llm_name}'")
