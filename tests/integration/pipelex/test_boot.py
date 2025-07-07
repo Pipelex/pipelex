39e2b1058b9fb5cb3c8d1752a8b5caee1a6e7164
thomashebrard <thomas@pipelex.com>
2025-07-07 09:25:35 +0200

    test_boot

--- tests/integration/pipelex/test_boot.py
+++ tests/integration/pipelex/test_boot.py
@@ -1,4 +1,12 @@
-def test_boot():
-    # This test does nothing but the conftest runs Pipelex.make().
-    # Therefore this test will fail if Pipelex.make() fails.
-    assert True
+import pytest
+
+from pipelex.hub import get_pipe_provider
+from pipelex.pipe_works.pipe_dry import dry_run_pipes
+
+
+@pytest.mark.asyncio(loop_scope="class")
+class TestBoot:
+    async def test_boot(self):
+        # This test does nothing but the conftest runs Pipelex.make() with the dryrun
+        # Therefore this test will fail if Pipelex.make() fails.
+        await dry_run_pipes(get_pipe_provider().get_pipes())
