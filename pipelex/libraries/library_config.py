c801efc09405ec0e8c386a467686654ece861651
Louis Choquel <louis@pipelex.com>
2025-09-16 00:47:32 +0200

    Remove more obsolete library copying plugins code

--- pipelex/libraries/library_config.py
+++ pipelex/libraries/library_config.py
@@ -11,20 +11,12 @@ class LibraryConfig(ConfigModel):
     config_dir_path: str = "pipelex_libraries"
 
     @property
-    def config_file_path(self) -> str:
-        return f"{self.config_dir_path}/config.toml"
-
-    @property
     def pipelines_dir_path(self) -> str:
         return f"{self.config_dir_path}/pipelines"
 
     @property
     def base_pipelines_dir_path(self) -> str:
         return f"{self.config_dir_path}/pipelines/base_library"
-
-    @property
-    def llm_integrations_dir_path(self) -> str:
-        return f"{self.config_dir_path}/llm_integrations"
 
     @property
     def templates_dir_path(self) -> str:
@@ -43,6 +35,13 @@ class LibraryConfig(ConfigModel):
 
     def export_libraries(self, overwrite: bool = False) -> None:
         """Duplicate pipelex libraries files in the client project, preserving directory structure."""
+        copy_file_from_package(
+            package_name=self.package_name,
+            file_path_in_package=f"{PIPELEX_LIBRARIES_PATH}/__init__.py",
+            target_path=f"{self.config_dir_path}/__init__.py",
+            overwrite=overwrite,
+        )
+
         # pipelines
         copy_folder_from_package(
             package_name=self.package_name,
@@ -52,27 +51,8 @@ class LibraryConfig(ConfigModel):
         )
         copy_file_from_package(
             package_name=self.package_name,
-            file_path_in_package=f"{PIPELEX_LIBRARIES_PATH}/__init__.py",
-            target_path=f"{self.config_dir_path}/__init__.py",
-            overwrite=overwrite,
-        )
-        copy_file_from_package(
-            package_name=self.package_name,
             file_path_in_package=f"{PIPELEX_LIBRARIES_PATH}/pipelines/__init__.py",
             target_path=f"{self.pipelines_dir_path}/__init__.py",
-            overwrite=overwrite,
-        )
-        copy_file_from_package(
-            package_name=self.package_name,
-            file_path_in_package=f"{PIPELEX_LIBRARIES_PATH}/pipelines/__init__.py",
-            target_path=f"{self.base_pipelines_dir_path}/__init__.py",
-            overwrite=overwrite,
-        )
-        # llm_integrations
-        copy_folder_from_package(
-            package_name=self.package_name,
-            folder_path_in_package=f"{PIPELEX_LIBRARIES_PATH}/llm_integrations",
-            target_dir=self.llm_integrations_dir_path,
             overwrite=overwrite,
         )
 
