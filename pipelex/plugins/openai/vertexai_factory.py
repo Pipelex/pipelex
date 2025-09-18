185c9f7f79c15e3c71f79658bcc608da5ab39a4b
Louis Choquel <louis@pipelex.com>
2025-09-18 12:19:29 +0200

    origin/fix/VertexAIFactory-without-credentials-2

--- pipelex/plugins/openai/vertexai_factory.py
+++ pipelex/plugins/openai/vertexai_factory.py
@@ -42,7 +42,7 @@ class VertexAIFactory(ConfigModel):
 
         gcp_credentials_file_path = extra_config.get(VertexAIExtraField.GCP_CREDENTIALS_FILE_PATH)
         if not gcp_credentials_file_path or gcp_credentials_file_path == ENV_DUMMY_PLACEHOLDER_VALUE:
-            raise VertexAIConfigError(f"GCP credentials file path is not properly set for VertexAI. Run mode is: '{runtime_manager.run_mode}'")
+            raise VertexAIConfigError(f"GCP credentials file path is not properly set for VertexAI. Run mode: '{runtime_manager.run_mode}'")
 
         api_key = cls._make_api_key(gcp_credentials_file_path=gcp_credentials_file_path)
 
