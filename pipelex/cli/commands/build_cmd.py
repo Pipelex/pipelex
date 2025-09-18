7d95f430aea1b32001c600913520776c3e4b8e9d
Louis Choquel <louis@pipelex.com>
2025-09-18 15:44:43 +0200

    build commands and 5 fields

--- pipelex/cli/commands/build_cmd.py
+++ pipelex/cli/commands/build_cmd.py
@@ -14,10 +14,10 @@ from pipelex.pipeline.execute import execute_pipeline
 build_app = typer.Typer(help="Build artifacts like pipelines", no_args_is_help=True)
 
 """
-Exmaples:
-pipelex build pipe "Take a photo as input, and render the opposite of the photo" -o ./built.plx
-pipelex build pipe "Given a invoice pdf, extract employee and articles" -o ./built.plx
-pipelex build pipe "Given an RDFP PDF, build a compliance matrix" -o ./built.plx
+Examples:
+pipelex build pipe "Take a photo as input, and render the opposite of the photo"
+pipelex build pipe "Given a scanned invoice, extract employee and articles"
+pipelex build pipe "Given an RDFP PDF, build a compliance matrix"
 """
 
 
