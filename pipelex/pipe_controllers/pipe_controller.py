c8bf98fd0676e5fa56974456b2aac4340ba43980
thomashebrard <thomas@pipelex.com>
2025-09-26 16:27:03 +0200

    specific pipes clean

--- pipelex/pipe_controllers/pipe_controller.py
+++ pipelex/pipe_controllers/pipe_controller.py
@@ -11,13 +11,6 @@ from pipelex.core.pipes.pipe_run_params import PipeRunMode, PipeRunParams
 from pipelex.pipeline.job_metadata import JobMetadata
 from pipelex.types import StrEnum
 
-
-class SpecificPipeCodesEnum(StrEnum):
-    CONTINUE = "continue"
-    # TODO: Implement the break pipe: It should enable to leave the current sequence.
-    # BREAK = "break"
-
-
 class PipeController(PipeAbstract):
     category: Literal["PipeController"] = "PipeController"
 
