3f16a6948dbd211ffce0f5f017bea7710bbd3c68
thomashebrard <thomas@pipelex.com>
2025-09-26 20:21:56 +0200

    fix StrEnum

--- pipelex/types.py
+++ pipelex/types.py
@@ -3,6 +3,6 @@ import sys
 if sys.version_info >= (3, 11):  # real std-lib
     from enum import StrEnum
 else:  # back-port
-    from enum import StrEnum
+    from backports.strenum import StrEnum
 
 __all__ = ["StrEnum"]
