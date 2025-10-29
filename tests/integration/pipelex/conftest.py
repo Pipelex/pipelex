8a5d20f1e67fdf8082c991dc63ef3e53d662a72d
Louis Choquel <louis@pipelex.com>
2025-10-29 12:13:21 -0700

    enable models

--- tests/integration/pipelex/conftest.py
+++ tests/integration/pipelex/conftest.py
@@ -193,12 +193,12 @@ def llm_job_params(request: pytest.FixtureRequest) -> LLMJobParams:
 @pytest.fixture(
     params=[
         # "flux-pro",
-        # "flux-pro/v1.1",
+        "flux-pro/v1.1",
         # "flux-pro/v1.1-ultra",
         # "fast-lightning-sdxl",
         # "gpt-image-1",
         # "nano-banana",
-        "best-img-gen",
+        # "best-img-gen",
     ],
 )
 def img_gen_handle(request: pytest.FixtureRequest) -> str:
