2996376b7cc2bf1bd114f0ab157520aa966e46c0
thomashebrard <thomas@pipelex.com>
2025-11-04 13:47:25 +0100

    make cc

--- pipelex/cogt/models/model_deck.py
+++ pipelex/cogt/models/model_deck.py
@@ -187,9 +187,10 @@ class ModelDeck(ConfigModel):
     def validate_llm_presets(self) -> Self:
         for llm_preset_id, llm_setting in self.llm_presets.items():
             if not self.is_model_handle_defined(model_handle=llm_setting.model):
+                enabled_backends = {model.backend_name for model in self.inference_models.values()}
                 msg = (
                     f"llm_handle '{llm_setting.model}' for llm_preset '{llm_preset_id}' not found in deck. "
-                    f"The enabled backends are {set([model.backend_name for model in self.inference_models.values()])}. "
+                    f"The enabled backends are {enabled_backends}. "
                     f"And the llm_handle '{llm_setting.model}' is not found in any of the enabled backends. Here are the solutions:\n "
                     "1 - Add it to your enabled backends if possible\n "
                     "2 - Enable a backend that supports this llm_handle "
