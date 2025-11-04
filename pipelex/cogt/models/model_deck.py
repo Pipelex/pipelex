8ddee94e38dbf2ef8726fbe24b36ebc2bd096854
thomashebrard <thomas@pipelex.com>
2025-11-04 14:21:39 +0100

    better woding

--- pipelex/cogt/models/model_deck.py
+++ pipelex/cogt/models/model_deck.py
@@ -192,7 +192,7 @@ class ModelDeck(ConfigModel):
                     f"llm_handle '{llm_setting.model}' for llm_preset '{llm_preset_id}' not found in deck. "
                     f"The enabled backends are {enabled_backends}. "
                     f"And the llm_handle '{llm_setting.model}' is not found in any of the enabled backends. Here are the solutions:\n "
-                    "1 - Add it to your enabled backends if possible\n "
+                    f"1 - Configure '{llm_setting.model}' for one of your enabled backends if possible\n"
                     "2 - Enable a backend that supports this llm_handle "
                 )
                 raise LLMHandleNotFoundError(msg)
