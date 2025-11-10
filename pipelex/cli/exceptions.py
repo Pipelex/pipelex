4887314b191f7975c6b1a09ba9c3135870fdc198
thomashebrard <thomas@pipelex.com>
2025-11-10 12:00:13 +0100

    make cc

--- pipelex/cli/exceptions.py
+++ pipelex/cli/exceptions.py
@@ -5,3 +5,7 @@ from pipelex.exceptions import PipelexException
 
 class PipelexCLIError(PipelexException, ClickException):
     """Raised when there's an error in CLI usage or operation."""
+
+
+class ReadinessCheckError(PipelexCLIError):
+    """Raised when readiness checks fail (missing dependencies or dev mode without venv)."""
