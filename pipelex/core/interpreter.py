97a49ab02edef58f6ed0f9052f557aa4be5b88bc
thomashebrard <thomas@pipelex.com>
2025-11-19 11:23:05 +0100

    add todo

--- pipelex/core/interpreter.py
+++ pipelex/core/interpreter.py
@@ -70,6 +70,7 @@ class PipelexInterpreter(BaseModel):
         try:
             return PipelexBundleBlueprint.model_validate(blueprint_dict)
         except ValidationError as exc:
+            #TODO: Move this to the validate_bundle function
             # Parse Pydantic validation errors into structured error data
             validation_errors: list[PipelexBundleBlueprintValidationErrorData] = []
 
