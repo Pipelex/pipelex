d04c409c1253c483c32ed914692563618c5f84ce
Louis Choquel <louis@pipelex.com>
2025-11-22 14:08:41 +0100

    New helper is_env_var_truthy(). Used to detect we are in Codex cloud and testing

--- pipelex/system/runtime.py
+++ pipelex/system/runtime.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from pipelex.system.environment import get_optional_env
+from pipelex.system.environment import get_optional_env, is_env_var_truthy
 from pipelex.types import StrEnum
 
 RUN_MODE_ENV_VAR_KEY = "RUN_MODE"
@@ -37,12 +37,17 @@ class RunMode(StrEnum):
     NORMAL = "normal"
     UNIT_TEST = "unit_test"
     CI_TEST = "ci_test"
+    CODEX_CLOUD = "codex_cloud"
+    CODEX_CLOUD_TEST = "codex_cloud_test"
 
     @classmethod
     def get_from_env_var(cls) -> "RunMode":
         if mode_str := get_optional_env(RUN_MODE_ENV_VAR_KEY):
             return RunMode(mode_str)
-        return RunMode.NORMAL
+        elif is_env_var_truthy(key="CODEX_CLOUD"):
+            return RunMode.CODEX_CLOUD
+        else:
+            return RunMode.NORMAL
 
 
 class WorkerMode(StrEnum):
@@ -109,9 +114,13 @@ class RuntimeManager(BaseModel):
         match self.run_mode:
             case RunMode.NORMAL:
                 return False
+            case RunMode.CODEX_CLOUD:
+                return False
             case RunMode.UNIT_TEST:
                 return True
             case RunMode.CI_TEST:
+                return True
+            case RunMode.CODEX_CLOUD_TEST:
                 return True
 
     @property
@@ -119,14 +128,14 @@ class RuntimeManager(BaseModel):
         match self.run_mode:
             case RunMode.NORMAL:
                 return False
+            case RunMode.CODEX_CLOUD:
+                return False
             case RunMode.UNIT_TEST:
                 return False
             case RunMode.CI_TEST:
                 return True
-
-    @property
-    def should_check_intermediate_configs(self) -> bool:
-        return self.is_unit_testing
+            case RunMode.CODEX_CLOUD_TEST:
+                return True
 
 
 runtime_manager = RuntimeManager()
