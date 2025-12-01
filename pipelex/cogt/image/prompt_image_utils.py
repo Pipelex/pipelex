ee545f760311e9f9498539ebd2ad7a18592a9b69
Louis Choquel <louis@pipelex.com>
2025-12-01 19:00:37 +0100

    Clarify image uri

--- pipelex/cogt/image/prompt_image_utils.py
+++ pipelex/cogt/image/prompt_image_utils.py
@@ -15,6 +15,7 @@ from pipelex.tools.misc.base_64_utils import (
     load_binary_async,
 )
 from pipelex.tools.misc.filetype_utils import detect_file_type_from_base64
+from pipelex.tools.misc.path_utils import clarify_path_or_url
 
 
 async def promptimage_to_b64_async(prompt_image: PromptImage) -> bytes:
@@ -53,12 +54,20 @@ async def promptimage_to_typed_bytes_or_url(
     if isinstance(prompt_image, PromptImageBase64):
         typed_bytes_or_url = prompt_image.make_prompt_image_typed_base64()
     elif isinstance(prompt_image, PromptImageUrl):
-        if is_http_url_enabled:
-            typed_bytes_or_url = prompt_image.url
+        file_path, url = clarify_path_or_url(path_or_uri=prompt_image.url)
+        if url:
+            if is_http_url_enabled:
+                typed_bytes_or_url = prompt_image.url
+            else:
+                image_bytes = await PromptImageFactory.make_promptimagebase64_from_url_async(prompt_image_url=prompt_image)
+                file_type = detect_file_type_from_base64(image_bytes.base_64)
+                typed_bytes_or_url = PromptImageTypedBase64(base_64=image_bytes.base_64, file_type=file_type)
+        elif file_path:
+            prompt_image_path = PromptImagePath(file_path=file_path)
+            return await promptimage_to_typed_bytes_or_url(prompt_image=prompt_image_path, is_http_url_enabled=is_http_url_enabled)
         else:
-            image_bytes = await PromptImageFactory.make_promptimagebase64_from_url_async(prompt_image_url=prompt_image)
-            file_type = detect_file_type_from_base64(image_bytes.base_64)
-            typed_bytes_or_url = PromptImageTypedBase64(base_64=image_bytes.base_64, file_type=file_type)
+            msg = "No file path or URL found"
+            raise PromptImageFactoryError(msg)
     elif isinstance(prompt_image, PromptImagePath):
         b64 = await load_binary_as_base64_async(prompt_image.file_path)
         typed_bytes_or_url = PromptImageTypedBase64(base_64=b64, file_type=prompt_image.get_file_type())
