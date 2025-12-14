6f25a888f4ea4ccc37e6fd068da5779d93ab96b3
Louis Choquel <8851983+lchoquel@users.noreply.github.com>
2025-12-14 15:37:18 +0100

    Gateway banana OK, blackbox broken

--- pipelex/plugins/openai/openai_img_gen_alt_worker.py
+++ pipelex/plugins/openai/openai_img_gen_alt_worker.py
@@ -1,11 +1,11 @@
 import random
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, cast
 
 import openai
 from openai import APIConnectionError, BadRequestError, NotFoundError
 from typing_extensions import override
 
-from pipelex import log
+from pipelex import log, pretty_print
 from pipelex.cogt.exceptions import LLMCompletionError, LLMModelNotFoundError, SdkTypeError
 from pipelex.cogt.image.generated_image import GeneratedImage
 from pipelex.cogt.img_gen.img_gen_job import ImgGenJob
@@ -44,7 +44,7 @@ class OpenAIImgGenAlternativeWorker(ImgGenWorkerAbstract):
         log.debug(f"Generating image with model: {self.inference_model.tag}")
         img_gen_prompt_text = img_gen_job.img_gen_prompt.positive_text
         messages = [{"role": "user", "content": img_gen_prompt_text}]
-        seed = img_gen_job.job_params.seed or random.randint(0, 2**32 - 1)
+        # seed = img_gen_job.job_params.seed or random.randint(0, 2**32 - 1)
         try:
             extra_headers, extra_body = self.openai_completions_factory.make_extras(
                 inference_model=self.inference_model, inference_job=img_gen_job, output_desc=InferenceOutputType.IMAGE
@@ -52,7 +52,7 @@ class OpenAIImgGenAlternativeWorker(ImgGenWorkerAbstract):
             response = await self.openai_client.chat.completions.create(
                 model=self.inference_model.model_id,
                 messages=messages,  # type: ignore[arg-type]
-                seed=seed,
+                # seed=seed,
                 extra_headers=extra_headers,
                 extra_body=extra_body,
             )
@@ -68,12 +68,23 @@ class OpenAIImgGenAlternativeWorker(ImgGenWorkerAbstract):
             raise LLMCompletionError(msg) from bad_request_error
 
         openai_message: ChatCompletionMessage = response.choices[0].message
-        response_text = openai_message.content
-        if response_text is None:
-            msg = f"OpenAI response message content is None: {response}\nmodel: {self.inference_model.desc}"
+        pretty_print(openai_message, title="openai_message")
+        url: str | None = None
+        if (content := openai_message.content) and content.startswith("http"):
+            url = openai_message.content
+        elif hasattr(openai_message, "content_blocks"):
+            content_blocks = cast("list[dict[str, Any]]", openai_message.content_blocks)  # pyright: ignore[reportAttributeAccessIssue, reportUnknownVariableType]
+            for part in content_blocks:
+                if part.get("type") == "image_url":
+                    if image_url := part.get("image_url"):
+                        if the_url := image_url.get("url"):
+                            url = the_url
+                            break
+        if not url:
+            msg = f"OpenAI response has no image. Model: {self.inference_model.desc}"
             raise LLMCompletionError(msg)
         return GeneratedImage(
-            url=response_text,
+            url=url,
             width=1024,
             height=1024,
         )
