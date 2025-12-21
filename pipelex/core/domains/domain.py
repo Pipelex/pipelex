cd626a023c9ccee77d74f09f2fc863b1bd059e82
thomashebrard <thomas@pipelex.com>
2025-12-21 13:44:11 +0100

    add validation to domain

--- pipelex/core/domains/domain.py
+++ pipelex/core/domains/domain.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
+from pipelex.core.domains.exceptions import DomainCodeError
+from pipelex.core.domains.validation import validate_domain_code
 from pipelex.types import StrEnum
 
 
@@ -24,3 +26,13 @@ class Domain(BaseModel):
     code: str
     description: str | None = None
     system_prompt: str | None = None
+
+    @field_validator("domain", mode="before")
+    @classmethod
+    def validate_domain_syntax(cls, domain_code: str) -> str:
+        try:
+            validate_domain_code(code=domain_code)
+        except DomainCodeError as exc:
+            msg = f"Error when trying to validate the pipelex bundle at domain '{domain_code}': {exc}"
+            raise ValueError(msg) from exc
+        return domain_code
