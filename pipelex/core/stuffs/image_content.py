8627129160651cd7c2d0f7424a49ba3899788469
Louis Choquel <8851983+lchoquel@users.noreply.github.com>
2026-01-07 13:20:48 +0100

    Render ImageContent html using display_link

--- pipelex/core/stuffs/image_content.py
+++ pipelex/core/stuffs/image_content.py
@@ -39,7 +39,7 @@ class ImageContent(StuffContent):
             template_source=template_source,
             template_category=TemplateCategory.HTML,
             temlating_context={
-                "url": self.url,
+                "url": self.display_link,
             },
         )
 
