92c184c56a78832e125d973c2847b75969453be4
Louis Choquel <lchoquel@users.noreply.github.com>
2026-01-31 12:39:12 +0100

    Lean extract model deck

--- tests/integration/pipelex/fixtures/extract_fixtures.py
+++ tests/integration/pipelex/fixtures/extract_fixtures.py
@@ -77,8 +77,8 @@ def extract_handle_from_image(request: pytest.FixtureRequest) -> str:
 
 @pytest.fixture(
     params=[
-        "$extract-all-from-document",
-        "$extract-text-from-pdf",
+        "@default-extract-document",
+        "@default-text-from-pdf",
     ],
 )
 def extract_choice_for_pdf(request: pytest.FixtureRequest) -> str:
@@ -88,7 +88,7 @@ def extract_choice_for_pdf(request: pytest.FixtureRequest) -> str:
 
 @pytest.fixture(
     params=[
-        "$extract-all-from-document",
+        "@default-extract-document",
     ],
 )
 def extract_choice_for_image(request: pytest.FixtureRequest) -> str:
