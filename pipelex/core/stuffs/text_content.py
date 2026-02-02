6bc522eb222d87c1a9891b84a7c05366f1c6d9a1
Louis Choquel <lchoquel@users.noreply.github.com>
2026-02-02 11:31:22 +0100

    Cleanup rogue html non escaping

--- pipelex/core/stuffs/text_content.py
+++ pipelex/core/stuffs/text_content.py
@@ -26,12 +26,9 @@ class TextContent(StuffContent):
 
     @override
     def rendered_html(self) -> str:
-        # If text looks like HTML, return it as-is (trusted HTML content from templates)
-        # Otherwise, escape HTML special characters so text displays literally in browsers
-        if self._looks_like_html():
-            return self.text
-        else:
-            return html.escape(self.text)
+        # Always escape HTML special characters so text displays literally in browsers.
+        # If you need to render trusted HTML content, use HtmlContent instead.
+        return html.escape(self.text)
 
     @override
     def rendered_markdown(self, level: int = 1, is_pretty: bool = False) -> str:
@@ -41,7 +38,6 @@ class TextContent(StuffContent):
     def rendered_json(self) -> str:
         return json.dumps({"text": self.text})
 
-    # TODO: This should not exist: In PipeCompose, if the category is HTML, the user should be recommended to use HtmlContent instead.
     def _looks_like_html(self) -> bool:
         """Check if the text content appears to be HTML."""
         return bool(HTML_PATTERN.match(self.text))
