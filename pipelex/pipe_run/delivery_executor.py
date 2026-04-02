52fe987d4cfdbcbb944ec68cc2ca7dd08cae3067
thomashebrard <thomas@pipelex.com>
2026-04-02 11:51:38 +0200

    fix s3 signature

--- pipelex/pipe_run/delivery_executor.py
+++ pipelex/pipe_run/delivery_executor.py
@@ -11,6 +11,7 @@ from pipelex.graph.graph_factory import generate_graph_outputs
 from pipelex.hub import get_storage_provider
 from pipelex.pipe_run.exceptions import StorageDeliveryError, WebhookDeliveryError
 from pipelex.tools.misc.json_utils import clean_json_dumps
+from pipelex.tools.storage.storage_provider_abstract import PIPELEX_STORAGE_SCHEME
 
 if TYPE_CHECKING:
     from pipelex.core.pipes.pipe_output import PipeOutput
@@ -126,7 +127,8 @@ class DeliveryExecutor:
                 await storage_provider.store(data=data, key=key, content_type=content_type)
                 log.debug(f"Stored: {key}")
 
-            public = await storage_provider.public_url(base_key)
+            base_uri = f"{PIPELEX_STORAGE_SCHEME}{base_key}"
+            public = await storage_provider.public_url(base_uri)
             result_url: str = f"{public or base_key}/"
             log.info(f"Storage delivery completed: pipeline_run_id={pipeline_run_id}, files={len(result_files)}")
             return result_url
